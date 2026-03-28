247cef9edffa7e2a8a3221f09a0e0cc8e10b5c6f
intothebluelake <leggieroandrea02@gmai.com>
2026-03-28 22:00:02 +0100

    Correzione baseUrl e index

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "intothebluelake.github.io/whatthedeepseathinks",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
