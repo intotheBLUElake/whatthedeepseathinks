751f5f8e13960181dd2e94e2c81ee772dd99436a
intothebluelake <leggieroandrea02@gmai.com>
2026-03-29 21:31:15 +0200

    Quartz sync: Mar 29, 2026, 9:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -56,6 +56,7 @@ const config: QuartzConfig = {
   plugins: {
     transformers: [
       Plugin.FrontMatter(),
+      Plugin.HardLineBreaks(), // <--- Aggiungi questa riga
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "git", "filesystem"],
       }),
